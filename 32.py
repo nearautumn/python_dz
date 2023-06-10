b5c7bf076373ad2c31a332baeb87f24eb0b6654a
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-06-10 18:30:01 +0300

    example 32 added propertly

--- 32.py
+++ 32.py
@@ -3,3 +3,22 @@
 меньше заданного минимума и не больше заданного
 максимума)'''
 
+def new_list() ->list:
+    lenght = int(input('Введите длину списка: '))
+    result_list = [int(input('Введите число: ')) for i in range(lenght)]
+    return result_list
+
+def in_range_indexes(user_list: list, user_min: int, user_max: int) -> list:
+    result = []
+    for i in range(len(user_list)):
+        if user_list[i] >= user_min and user_list[i] <= user_max:
+            result.append(i)
+    return result
+
+
+list_1 = new_list()
+
+min = int(input('Введите минимум: '))
+max = int(input('Введите максимум: '))
+
+print('Индексы чисел, входящих в диапазон:', *in_range_indexes(list_1, min, max))
