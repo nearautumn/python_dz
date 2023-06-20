7bb745ad2e252e34677f4334e859b1c7d12d651a
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-06-20 20:19:54 +0300

    updated

--- 38_phonebook/main.py
+++ 38_phonebook/main.py
@@ -11,20 +11,25 @@ def main():
     if select == 1:
         print('Ваши контакты: ')
         show_all_records(file_path)
+        main()
     elif select == 2:
         name = input('Введите фамилию: ')
         print(search_record(file_path, name))
+        main()
     elif select == 3:
         add_contact(file_path)
         print('Контакт добавлен!')
+        main()
     elif select == 4:
         show_all_records(file_path)
         edit_record(file_path)
         print('Контакт изменен!')
+        main()
     elif select == 5:
         show_all_records(file_path)
         delete_record(file_path)
         print('Контакт удален!')
+        main()
     else:
         print('Некорректный ввод')
         main()
